93bbe8324c4a5ac48e1d63f78281778f8873f2dd
Varun Chimatavarun <chimatavarun1@gmail.com>
2025-08-16 00:21:40 +0530

    fix: Set basename for react-router on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
 
-createRoot(document.getElementById("root")!).render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Assuming you're using this
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* === ADD THIS 'basename' PROP === */}
+    <BrowserRouter basename="/varun-portfolio/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
